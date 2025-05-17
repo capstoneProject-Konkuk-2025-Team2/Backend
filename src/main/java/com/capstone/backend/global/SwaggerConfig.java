23cea0bc133b321b18586dbc2a51bd991da4c3f1
김영록 <praoo800@konkuk.ac.kr>
2025-05-17 09:33:34 +0900

    feat : swagger 환경 구축

---
+++ src/main/java/com/capstone/backend/global/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.capstone.backend.global;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Server")
+                ))
+                .components(components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Capstone Design API Docs")
+                .description("졸업프로젝트 관련 spring 서버 Api Document 입니다.");
+    }
+
+    // JWT 토큰을 위한 보안 컴포넌트 설정
+    private Components components() {
+        String securityScheme = "JWT TOKEN";
+        return new Components()
+                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                        .name(securityScheme)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+    }
+
+    // 보안 요구사항 설정
+    private SecurityRequirement securityRequirement() {
+        String securityScheme = "JWT TOKEN";
+        return new SecurityRequirement().addList(securityScheme);
+    }
+}

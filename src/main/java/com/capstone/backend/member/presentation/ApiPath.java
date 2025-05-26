0a3c99ed67699bb01232a3c54458e09f8fd04dae
김영록 <praoo800@konkuk.ac.kr>
2025-05-27 01:53:49 +0900

    feat : 관심정보 입력 받기 API 구현 #13

--- src/main/java/com/capstone/backend/member/presentation/ApiPath.java
+++ src/main/java/com/capstone/backend/member/presentation/ApiPath.java
@@ -9,4 +9,5 @@ public final class ApiPath {
     public static final String VERIFY_AUTH_CODE = "/v1/member/auth-code";
     public static final String SETTING_PASSWORD = "/v1/member/password";
     public static final String MAKE_TIMETABLE = "/v1/member/timetable";
+    public static final String SETTING_INTEREST = "/v1/member/interest";
 }
